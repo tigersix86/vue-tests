278f5fa0dd470408669c84086cce4192b2d8d976
Magnus Nordin <magnus.nordin@kvartsgatan13.se>
2019-02-15 14:32:31 +0100

    first commit

---
+++ skills/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Hello {{ their_name }}, this is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+        /* Funkar inte av n�gon anledning, vid "falskt" returneras inte 'Hi' */
+      their_name: this.$route.params.name != '' ? 'true' : 'Hi'
+    }
+  }
+}
+</script>
